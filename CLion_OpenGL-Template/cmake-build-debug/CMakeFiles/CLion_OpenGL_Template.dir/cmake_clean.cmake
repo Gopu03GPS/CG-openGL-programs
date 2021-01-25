2b835d7b710029ef0b6e1efe01c6d29e7f1945a8
The-Anton <shekhar59324@gmail.com>
2021-01-25 16:10:53 +0530

    Template for CLion

---
+++ CLion_OpenGL-Template/cmake-build-debug/CMakeFiles/CLion_OpenGL_Template.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CLion_OpenGL_Template"
+  "CLion_OpenGL_Template.pdb"
+  "CMakeFiles/CLion_OpenGL_Template.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_OpenGL_Template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
